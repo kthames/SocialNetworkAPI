4a9576616d525aa8c0ea7ef531ee1e881649d04d
kthames <135296109+kthames@users.noreply.github.com>
2023-10-17 11:36:19 -0500

    basic file structure added

---
+++ models/Thought.js
@@ -0,0 +1,54 @@
+
+// * `thoughtText`
+//   * String
+//   * Required
+//   * Must be between 1 and 280 characters
+
+// * `createdAt`
+//   * Date
+//   * Set default value to the current timestamp
+//   * Use a getter method to format the timestamp on query
+
+// * `username` (The user that created this thought)
+//   * String
+//   * Required
+
+// * `reactions` (These are like replies)
+//   * Array of nested documents created with the `reactionSchema`
+
+// **Schema Settings**:
+
+// Create a virtual called `reactionCount` that retrieves the length of the thought's `reactions` array field on query.
+
+// ---
+
+const {Schema, model} = require('mongoose');
+
+reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280,
+            minlength: 1,
+            default: 'Empty thought',
+          },
+          createdAt: {
+            type: Date,
+            default: Date.now,
+          },
+          username: {
+            type: String, 
+            required: 'Please provide a username',
+          }, 
+          reactions: [reactionSchema]
+    }, 
+    {
+        toJSON: {
+          virtuals: true,
+        },
+        id: false,
+    }
+);
---
+++ models/User.js
@@ -0,0 +1,67 @@
+// * `username`
+//   * String
+//   * Unique
+//   * Required
+//   * Trimmed
+
+// * `email`
+//   * String
+//   * Required
+//   * Unique
+//   * Must match a valid email address (look into Mongoose's matching validation)
+
+// * `thoughts`
+//   * Array of `_id` values referencing the `Thought` model
+
+// * `friends`
+//   * Array of `_id` values referencing the `User` model (self-reference)
+
+// **Schema Settings**:
+
+// Create a virtual called `friendCount` that retrieves the length of the user's `friends` array field on query.
+
+const {Schema, model} = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String, 
+      unique: true,
+      required: 'Please provide a username',
+      trim: true,  
+    }, 
+    email: {
+      type: String,
+      required: 'Please provide an email',
+      match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+      unique: true, 
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+  }, 
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendsCount').get(function () {
+  return this.friends.length;
+})
+
+const User = model('user', userSchema);
+
+module.exports = User;
